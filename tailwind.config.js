2730ce6d76b04f17b37b60bbe9f62d73e8528ed1
unknown <pradanahafiidh>
2023-07-19 01:31:54 +0700

    Huddle Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "18px",
+    },
+    extend: {
+      color: {
+        primary :"#083344",
+        secondary: "#db2777"
+      },
+      screens:{
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+}
+
